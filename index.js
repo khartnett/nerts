3d92ba33c1f8e76e2e95444d9873839b953dc500
Kieran Hartnett <khartnet@gmail.com>
2017-08-09 19:45:58 -0700

    more socket fun

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.get('/pixijs', function(req, res){
 
 io.on('connection', function(socket){
     socket.on('chat message', function(msg){
-        io.emit('chat message', msg);
+        io.emit('chat message', msg + socket.id);
     });
 });
 
