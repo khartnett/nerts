e99c09e1f8b1f0e19b381092f3bdd69243b4883e
Kieran Hartnett <khartnet@gmail.com>
2017-08-09 19:18:34 -0700

    added node and chat

---
+++ index.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var port = process.env.PORT || 3000;
+
+app.use(express.static('public'))
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/pixijs', function(req, res){
+    res.sendFile(__dirname + '/indexPixi.html');
+});
+
+io.on('connection', function(socket){
+    socket.on('chat message', function(msg){
+        io.emit('chat message', msg);
+    });
+});
+
+http.listen(port, function(){
+    console.log('listening on *:' + port);
+});
